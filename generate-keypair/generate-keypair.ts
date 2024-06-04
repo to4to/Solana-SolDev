29769508f4c4cbfba1e1645be51894faa43a7b02
to4to <justjustifying@outlook.com>
2024-06-04 13:50:53 +0530

    keypair

---
+++ generate-keypair/generate-keypair.ts
@@ -0,0 +1,20 @@
+import "dotenv/config"
+import {Keypair} from "@solana/web3.js"
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair= Keypair.generate();
+
+
+console.log(`Key PAir Generated`);
+
+
+
+console.log(`The public key is: `, keypair.publicKey.toBase58());
+console.log(`The secret key is: `, keypair.secretKey);
+console.log(`✅ Finished!`);
+
+
+const keyPair = getKeypairFromEnvironment("SECRET_KEY");
+console.log(
+    `✅ Finished! We've loaded our secret key securely, using an env file!`
+  );
